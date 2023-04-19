d7e4f5624e2689cebef72f3bd602a317f752d166
TheNesh <The.Nesh@yandex.ru>
2023-04-19 21:10:05 +0300

    homePage

--- public/homePage.js
+++ public/homePage.js
@@ -1,6 +1,7 @@
 const logoutButton = new LogoutButton;
 const ratesBoard = new RatesBoard;
 const moneyManager = new MoneyManager;
+const favoritesWidget = new FavoritesWidget;
 
 logoutButton.action = () => {
   ApiConnector.logout((element) => {
@@ -35,6 +36,68 @@ moneyManager.addMoneyCallback = (data) => {
       moneyManager.setMessage(response.success, "Баланс пополнен");
     } else {
       moneyManager.setMessage(response.success, response.error);
+    }
+  });
+}
+
+moneyManager.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(
+        response.success,
+        "Конвертация успешно завершена"
+      );
+    } else {
+      moneyManager.setMessage(response.success, response.error);
+    }
+  });
+}
+
+moneyManager.sendMoneyCallback = (data) => {
+  ApiConnector.transferMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(
+        response.success,
+        "Перевод успешно выполнен"
+      );
+    } else {
+      neyManager.setMessage(response.success, response.error);
+    }
+  });
+}
+
+ApiConnector.getFavorites((response) => {
+  if (response) {
+    favoritesWidget.clearTable();
+    favoritesWidget.fillTable(response.data);
+    moneyManager.updateUsersList(response.data);
+  }
+});
+
+favoritesWidget.addUserCallback = (data) => {
+  ApiConnector.addUserToFavorites(data, (response) => {
+    if (response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+      favoritesWidget.setMessage(response.success, 'Пользователь добавлен');
+    } else {
+      favoritesWidget.setMessage(response.success, response.error);
+    }
+  });
+};
+
+favoritesWidget.removeUserCallback = (id) => {
+  ApiConnector.removeUserFromFavorites(id, (response) => {
+    if (response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+      favoritesWidget.setMessage(response.success, "Пользователь удален");
+    } else {
+      favoritesWidget.setMessage(response.success, response.error);
     }
   });
 }
